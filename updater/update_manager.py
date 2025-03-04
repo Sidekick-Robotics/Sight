f19087d3408518d039783568312dfe3d7de9aa86
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-03-04 00:14:36 +0000

    Update manager is functional!

--- updater/update_manager.py
+++ updater/update_manager.py
@@ -6,14 +6,14 @@ This is currently for windows only!
 """
 import os
 import sys
+import time
 import ctypes
 import shutil
 
 from dulwich.porcelain import clone
 
 # Git definitions
-GIT_EXE = f".\\Git\\cmd\\git.exe"
-GIT_REPO = f"\"https://github.com/Sidekick-Robotics/Sight-updates\""
+GIT_REPO = f"https://github.com/Sidekick-Robotics/Sight-updates.git"
 
 def requires_admin():
     """Check if the updater needs admin permissions."""
@@ -33,7 +33,8 @@ def is_admin():
 
 def run_as_admin():
     """Relaunch the script with admin privileges using UAC."""
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    sys_args = f"\"{GIT_DES}\" {SEP}"
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, sys_args, None, 1)
 
 def update_sight():
     """Git clone the latest repository."""
@@ -43,7 +44,10 @@ if __name__ == "__main__":
     GIT_DES = sys.argv[1]
     SEP = sys.argv[2]
 
+    print(__file__)
     print(SEP, GIT_DES)
+
+    time.sleep(2)
 
     # Run as admin if needed
     if requires_admin() and not is_admin():
@@ -52,7 +56,13 @@ if __name__ == "__main__":
 
     # Manage the path if the path already exists
     if os.path.exists(GIT_DES):
-        shutil.rmtree(GIT_DES)
-        os.mkdir(GIT_DES)
+        try:
+            shutil.rmtree(GIT_DES)
+        except PermissionError:
+            pass
+        try:
+            os.mkdir(GIT_DES)
+        except FileExistsError:
+            pass
 
     update_sight()
